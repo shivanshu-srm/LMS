0530423b4e5c80ff55ed9c111468629d653c9520
SHIVANSHU DIXIT <shivanshudixit121@gmail.com>
2024-04-25 10:54:55 +0530

    all screens

---
+++ LMS/Screens/Collections.swift
@@ -0,0 +1,18 @@
+//
+//  Collections.swift
+//  LMS
+//
+//  Created by shivanshu on 25/04/24.
+//
+
+import SwiftUI
+
+struct Collections: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Collections()
+}
