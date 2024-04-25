2e86b715e78a2469d5968d91bbb237d740549517
SHIVANSHU DIXIT <shivanshudixit121@gmail.com>
2024-04-25 14:03:39 +0530

    Desciption screen

---
+++ LMS/Screens/BookDescription.swift
@@ -0,0 +1,18 @@
+//
+//  BookDescription.swift
+//  LMS
+//
+//  Created by shivanshu on 25/04/24.
+//
+
+import SwiftUI
+
+struct BookDescription: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BookDescription()
+}
