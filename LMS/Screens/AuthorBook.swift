52a1c26a65e26c3b8a1491caf7da84c07710b729
SHIVANSHU DIXIT <shivanshudixit121@gmail.com>
2024-04-26 17:10:21 +0530

    Filterscreen, author book screen

---
+++ LMS/Screens/AuthorBook.swift
@@ -0,0 +1,18 @@
+//
+//  AuthorBook.swift
+//  LMS
+//
+//  Created by shivanshu on 26/04/24.
+//
+
+import SwiftUI
+
+struct AuthorBook: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AuthorBook()
+}
---
+++ LMS/Screens/Filters.swift
@@ -0,0 +1,18 @@
+//
+//  Filters.swift
+//  LMS
+//
+//  Created by shivanshu on 26/04/24.
+//
+
+import SwiftUI
+
+struct Filters: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Filters()
+}
