c1337ca7c12986214d0388b448f842eb2f061d60
SHIVANSHU DIXIT <shivanshudixit121@gmail.com>
2024-04-29 11:04:42 +0530

    Feedback screen , and analytics screen.

---
+++ LMS/Screens/FeedbackView.swift
@@ -0,0 +1,18 @@
+//
+//  FeedbackView.swift
+//  LMS
+//
+//  Created by shivanshu on 29/04/24.
+//
+
+import SwiftUI
+
+struct FeedbackView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FeedbackView()
+}
