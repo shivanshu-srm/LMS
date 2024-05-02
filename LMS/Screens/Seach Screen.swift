fb7ff046979908c3d66a3a121d78af77ab07f9d7
SHIVANSHU DIXIT <shivanshudixit121@gmail.com>
2024-05-03 04:09:07 +0530

    Search functionality

---
+++ LMS/Screens/Seach Screen.swift
@@ -0,0 +1,18 @@
+//
+//  Seach Screen.swift
+//  LMS
+//
+//  Created by shivanshu on 02/05/24.
+//
+
+import SwiftUI
+
+struct Seach_Screen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Seach_Screen()
+}
---
+++ LMS/Screens/Search.swift
@@ -0,0 +1,18 @@
+//
+//  Search.swift
+//  LMS
+//
+//  Created by shivanshu on 02/05/24.
+//
+
+import SwiftUI
+
+struct Search: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Search()
+}
